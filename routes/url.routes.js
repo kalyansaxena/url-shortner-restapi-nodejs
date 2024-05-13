cfd558f060caa660f88807e3c92c6ebf15a3487b
Kalyan-Saxena <kalyansaxena123@gmail.com>
2024-05-13 23:22:41 +0530

    Initial release of url shortner rest api

---
+++ routes/url.routes.js
@@ -0,0 +1,24 @@
+import express from "express";
+import { createShortUrl, deleteUrl, getURL, getUrls, redirectUrl, updateUrl } from "../controllers/url.controllers.js";
+
+const router = express.Router();
+
+// Create short URL
+router.post("/create", createShortUrl);
+
+// GET all urls
+router.get("/", getUrls);
+
+// Get a specific URL by shorturl
+router.get("/:shortUrl", getURL)
+
+// Redirect to originalUrl
+router.get("/redirect/:shortUrl", redirectUrl);
+
+// update original url by shortUrl
+router.put("/:shortUrl", updateUrl);
+
+// Delete a URL by shortUrl
+router.delete("/:shortUrl", deleteUrl);
+
+export default router;
